a8eff011cc5c8f39f58e5b124a0eae76bdff70ec
stuart <stuartgough1@gmail.com>
2014-03-25 16:02:30 +1100

    CVJ-18 Custodian search functionality (creator_user_id)

---
+++ ckanext/extend_search/fanstatic/custodianpicker-module.js
@@ -0,0 +1,45 @@
+this.ckan.module('custodianpicker-module', function($, _) {
+    return {
+        initialize: function() {
+
+            // Add hidden <input> tag #ext_cust_id if it doesn't already exist.
+            var form = $(".search-form");
+            if ($("#ext_cust_id").length === 0) {
+                $('<input type="hidden" id="ext_cust_id" name="ext_cust_id" />').appendTo(form);
+            }
+
+            var ext_cust_id = null
+
+            //Display custodian search param after search ?
+            if(getURLParameter("ext_cust_id")) {
+                ext_cust_id = getURLParameter("ext_cust_id");
+                $('#ext_cust_id').val(ext_cust_id);
+                $('#ext_cust_after').text(ext_cust_id);
+            }
+
+            //Initialise selectlist with url param ext_cust_id
+            if(ext_cust_id)
+            {
+                //Initialise dropdown
+                $('#cust_id option[user_id="ext_cust_id"]')
+
+                //$('#cust_id option:eq(ext_cust_id)).attr('selected', "selected")
+            }
+
+            //Event listener for when a user is selected from the list
+            $("#cust_id").on('change', function () {
+
+                    $('#ext_cust_id').val($(this).attr('value'));
+
+                    // Submit the <form id="dataset-search">.
+                    $(".search-form").submit();
+                })
+
+            // Returns url parameter of given name (if it exists)
+            function getURLParameter(name) {
+                return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+            }
+
+        }
+    }
+});
--- ckanext/extend_search/plugin.py
+++ ckanext/extend_search/plugin.py
@@ -5,6 +5,7 @@ import ckan.model.meta as meta
 from ckan.lib.base import c
 
 
+
 class ExtendSearchPlugin(plugins.SingletonPlugin):
     '''Extends the Ckan dataset/package search
     '''
@@ -12,12 +13,20 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IPackageController, inherit=True)
     plugins.implements(plugins.IDatasetForm, inherit=True)
 
+    def get_user_name_by_id(id):
+
+        user = meta.Session.query(User).filter(User.id==id).first()
+
+        if(user):
+            return user.name
+
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_resource('fanstatic', 'ckanext-datesearch')
+        toolkit.add_resource('fanstatic', 'custodianpicker-module')
 
-
+    # Add the custom parameters to Solr's facet queries
     def before_search(self, search_params):
 
         extras = search_params.get('extras')
@@ -27,17 +36,34 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
 
         start_date = extras.get('ext_startdate')
         end_date = extras.get('ext_enddate')
+        cust_id = extras.get('ext_cust_id')
 
-        if not start_date or not end_date:
-            # The user didn't select a start and end date, so do nothing.
-            return search_params
+        if not cust_id:
+            print('no custodian id param')
+            if not start_date or not end_date:
+                # The user didn't select any additional params, so do nothing.
+                return search_params
 
-        # Add a date-range query with the selected start and end dates into the
-        # Solr facet queries.
         fq = search_params['fq']
-        fq = '{fq} +metadata_modified:[{start_date} TO {end_date}]'.format(
-            fq=fq, start_date=start_date, end_date=end_date)
 
+        if start_date and end_date:
+            # Add a date-range query with the selected start and end dates into the
+            # Solr facet queries.
+            fq = '{fq} +metadata_modified:[{start_date} TO {end_date}]'.format(
+                fq=fq, start_date=start_date, end_date=end_date)
+
+        #Add creator (user) id query to the Solr facet queries
+        if cust_id:
+            fq = '{fq} +creator_user_id:{cust_id}'.format(
+                fq=fq, cust_id=cust_id)
+
+            # user = meta.Session.query(User).filter(User.id==cust_id).first()
+            # if(user):
+            #     print(search_params['extras'])
+            #     search_params['extras'].update({'user_name':user.name})
+            #     print(search_params['extras'])
+
+        #return modified facet queries
         search_params['fq'] = fq
 
         return search_params
@@ -67,5 +93,6 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
     def is_fallback(self):
         return True
 
+    #Setup the model(s) to use in the template initialisation
     def setup_template_variables(self, context, data_dict=None, package_type=None):
         c.users = meta.Session.query(User).all()
